9c4542146eb2b6c6798b8a619e7ac7ed32af9bf3
Brian ONeil <brian@netadmin.com>
2024-12-13 05:35:19 +0000

    change import

--- train_gen.py
+++ train_gen.py
@@ -1,6 +1,5 @@
 import os
-import tensorflow as tf
-from tf.keras.models import Sequential
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
 from tensorflow.keras.callbacks import EarlyStopping
